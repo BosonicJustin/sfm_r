6f2c76b4b9d5bb17702fad29daf4123bdcfd4061
BosonicJustin <justinas.schwartz@gmail.com>
2024-08-11 19:38:44 +0200

    Add convolution

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::f32::consts::PI;
-use image::ImageReader;
+use image::{ImageBuffer, ImageReader, Luma};
 
 
 struct Gaussian {
@@ -19,9 +19,29 @@ impl Gaussian {
     }
 }
 
-fn main() {
-    let img = ImageReader::open("munich.png");
+fn convolve_image(image: &ImageBuffer<Luma<f32>, Vec<f32>>, kernel: &Gaussian, x_c: f32, y_c: f32) -> f32 {
+    let (width, height) = image.dimensions();
+    let mut sum: f32 = 0.0;
 
-    let g = Gaussian::new(1.0);
-    println!("{}", g.eval(0.0,0.0));
+    for j in 0..height - 1 {
+        for i in 0..width - 1 {
+            let pixel_val = image.get_pixel(i, j)[0];
+            let kernel_val = kernel.eval(x_c - i as f32, y_c - j as f32);
+
+            sum += pixel_val * kernel_val;
+        }
+    }
+
+    sum
+}
+
+fn main() {
+    let img = ImageReader::open("munich.png")
+        .unwrap().decode().unwrap().to_luma32f();
+
+    let (width, height) = img.dimensions();
+    // println!("Dimensions: {}x{}", width, height);
+
+    let result = convolve_image(&img, &Gaussian::new(1.0), 10.0, 15.0);
+
 }
